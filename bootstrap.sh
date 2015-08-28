7be75cc5df65317e1c0549b853b6fd53c5372dc5
fRzzy <frzzman@gmail.com>
2015-08-28 17:19:29 +0700

    stripped down

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,5 @@
 
 yum update -y
 yum groupinstall -y 'Development Tools'
-curl --silent --location https://rpm.nodesource.com/setup | bash -
-yum install -y nodejs
-npm install -g npm
-npm install -g bower grunt-cli gulp nodemon
-npm install -g yo generator-hottowel generator-gulp-webapp
+# npm install -g bower grunt-cli gulp nodemon
+# npm install -g yo generator-hottowel generator-gulp-webapp
