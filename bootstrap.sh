cc81aa3f778284abd3cdc610c5485bd7e08c9a8d
fRzzy <frzzman@gmail.com>
2015-08-28 12:07:56 +0700

    install nodejs from nodesource

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,7 @@
 
 yum update -y
 yum groupinstall -y 'Development Tools'
-yum install -y epel-release
+curl --silent --location https://rpm.nodesource.com/setup | bash -
 yum install -y nodejs
-yum install -y npm
 npm install -g bower grunt-cli gulp nodemon
 npm install -g yo generator-hottowel generator-gulp-webapp
