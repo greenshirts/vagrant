ba1ca63464591bcd627747c9a75c2e94085bb6c8
fRzzy <frzzman@gmail.com>
2015-08-28 12:18:57 +0700

    update npm to latest

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,6 @@ yum update -y
 yum groupinstall -y 'Development Tools'
 curl --silent --location https://rpm.nodesource.com/setup | bash -
 yum install -y nodejs
+npm install -g npm
 npm install -g bower grunt-cli gulp nodemon
 npm install -g yo generator-hottowel generator-gulp-webapp
