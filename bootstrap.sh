0937a13285b5c433db61866c2a4062eee37e555e
fRzzy <frzzman@gmail.com>
2015-08-31 00:16:34 +0700

    fix bootstrap wont install nodejs and npm automatically

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 yum update -y
 yum groupinstall -y 'Development Tools'
 yum install -y epel-release
-yum install nodejs npm
+yum install -y nodejs npm
 npm install -g npm
 npm install -g bower grunt-cli gulp
 # npm install -g nodemon yo generator-hottowel generator-gulp-webapp
