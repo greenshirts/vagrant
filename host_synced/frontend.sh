5dedbc64d23026bee4d159f084f8f2380674e4db
fRzzy <frzzman@gmail.com>
2015-09-16 14:30:34 +0700

    do not install yeoman & generators by default

--- host_synced/frontend.sh
+++ host_synced/frontend.sh
@@ -3,7 +3,7 @@
 yum install -y nodejs npm
 npm install -g npm
 npm install -g bower grunt-cli gulp nodemon
-yum install -y fontconfig
-npm install -g yo generator-hottowel generator-gulp-webapp
+# yum install -y fontconfig
+# npm install -g yo generator-hottowel generator-gulp-webapp
 
 hostnamectl set-hostname gs-fe.local
